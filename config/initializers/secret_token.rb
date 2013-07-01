4d791dafcc7184b91e8f23de04063909205cdb4f
Patricia Silva <patisilva@unknown84383541fd9c.att.net>
2013-07-01 14:57:17 -0500

    Initial commit. Starting all over

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PatiSilvaBlog::Application.config.secret_token = '309f7002a97554ca323c0e617b6199704078096fae13d6b238757ac9b7073b2c783b075364003549bd2ae5b6c4068a4b99c640bc1757fe04a05a11243e4cd15c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PatiSilvaBlog::Application.config.session_store :cookie_store, key: '_pati_silva_blog_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PatiSilvaBlog::Application.config.session_store :active_record_store
